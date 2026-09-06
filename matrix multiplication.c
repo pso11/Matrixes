44bf5733cf0d1e31b1cc9e93e13fc247e08b535e
pso11 <sofiya.pshenichnaya.09@mail.ru>
2026-09-06 23:51:34 +0300

    Теперь работает

--- matrix multiplication.c
+++ matrix multiplication.c
@@ -1,50 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <cstdlib>
 
-void print_matrix(int* matrix);
-void multiplication_2_matrix(int* matrix1, int* matrix2);
+void print_matrix(int* matrix, int lines, int columns);
+int* multiplication_2_matrix(int* matrix1, int* matrix2);
 
-const int lines = 5, columns = 4;
+const int lines_1 = 5, columns_1 = 5, lines_2 = 5, columns_2 = 5;
 
 int main(void)
 {
-    int matrix1[5][4] =
+    int matrix1[lines_1][columns_1] =
     {
-        {1, 2, 3, 4},
-        {11, 12, 13, 14}, 
-        {21, 22, 23, 24},
-        {31, 32, 33, 34},
-        {41, 42, 43, 44}
+        {1, 2, 3, 4, 5},
+        {11, 12, 13, 14, 15}, 
+        {21, 22, 23, 24, 25},
+        {31, 32, 33, 34, 35},
+        {41, 42, 43, 44, 45}
     };
-    int matrix2[4][5] =
+    int matrix2[lines_2][columns_2] =
     {
         {2, 3, 4, 5, 6},
-        {12, 13, 14, 15. 16}, 
+        {12, 13, 14, 15, 16}, 
         {22, 23, 24, 25, 25},
-        {32, 33, 34, 35, 36}
+        {32, 33, 34, 35, 36},
+        {42, 43, 44, 45, 46}
     };
 
     printf("Matrix 1\n\n");
-    print_matrix((int*)matrix1, lines, columns);
+    print_matrix((int*)matrix1, lines_1, columns_1);
 
     printf("Matrix 2\n\n");
-    print_matrix((int*)matrix2, lines, columns);
+    print_matrix((int*)matrix2, lines_2, columns_2);
 
     printf("Result of folding to matrixes:\n\n");
-    multiplication_2_matrix((int*)matrix1, (int*)matrix2, lines, columns);
+    print_matrix( multiplication_2_matrix((int*)matrix1, (int*)matrix2), lines_1, columns_2);
     return 0;
 }
 
 void print_matrix(int* matrix, int lines, int columns)
 {
+    if (columns_1 != lines_2)
+    {
+        printf("Error!!!! Wrong data");
+    }
     for (int y = 0; y < lines; y++)
     {
         for (int x = 0; x < columns; x++)
         {
-            for (int sum = 0, i = 0; i < 5; i++)
-            {
-                
-            }
             printf("%5d ", *(matrix + y * columns + x));
         }
         printf("\n");
@@ -52,24 +54,21 @@ void print_matrix(int* matrix, int lines, int columns)
     printf("\n");
 }
 
-void multiplication_2_matrix(int* matrix1, int* matrix2, int lines, int columns)
+int* multiplication_2_matrix(int* matrix1, int* matrix2)
 {
-    int matrix_result[5][5] = {};
-    for (int y = 0; y < lines; y++)
+    static int matrix_result[lines_1][columns_2] = {};
+    for (int y = 0; y < lines_1; y++)
     {
-        for (int x = 0; x < columns; x++)
+        for (int x = 0; x < columns_2; x++)
         { 
-            matrix_result[y][x] = *(matrix1 + y * columns + x) + *(matrix2 + y * columns + x);
+            for (int k = 0; k < columns_1; k++)
+            {
+                matrix_result[y][x] += (*(matrix1 + y * columns_1 + k) * *(matrix2 + k * columns_2 + x));
+            }
         }
     }
-
-    for (int y = 0; y < lines; y++)
-    {
-        for (int x = 0; x < columns; x++)
-        {
-            printf("%5d ", *((int*)matrix_result + y * columns + x));
-        }
-        printf("\n");
-    }
+    return (int*)matrix_result;
 }
+
+
 
