afbeafe6d316768c9447be017040b08fde6f2a4a
pso11 <sofiya.pshenichnaya.09@mail.ru>
2026-09-06 23:52:14 +0300

    Работает исправно

--- matrix_addition.c
+++ matrix_addition.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void print_matrix(int* matrix1);
-int* multiplication_2_matrix(int* matrix1, int* matrix2);
+void print_matrix(int* matrix);
+int* addition_2_matrix(int* matrix1, int* matrix2);
 
-const int lines = 5, columns = 5;
+const int lines = 5, columns = 4;
 
 int main(void)
 {
     int matrix1[lines][columns] =
     {
-        {1, 2, 3, 4, 5},
-        {11, 12, 13, 14, 15}, 
-        {21, 22, 23, 24, 16},
-        {31, 32, 33, 34, 16},
-        {41, 42, 43, 44, 45}
+        {0, 2, 3, 4},
+        {11, 12, 13, 14}, 
+        {21, 22, 23, 24},
+        {31, 32, 33, 34},
+        {41, 42, 43, 44}
     };
     int matrix2[lines][columns] =
     {
-        {2, 3, 4, 5, 6},
-        {12, 13, 14, 15, 16}, 
-        {22, 23, 24, 25, 26},
-        {32, 33, 34, 35, 36},
-        (42, 43, 44, 45, 46)
+        {2, 3, 4, 5},
+        {12, 13, 14, 15}, 
+        {22, 23, 24, 25},
+        {32, 33, 34, 35},
+        {42, 43, 44,45}
     };
 
     printf("Matrix 1\n\n");
@@ -32,7 +32,8 @@ int main(void)
     print_matrix((int*)matrix2);
 
     printf("Result of folding to matrixes:\n\n");
-    print_matrix( multiplication_2_matrix((int*)matrix1, (int*)matrix2) );
+    print_matrix( addition_2_matrix((int*)matrix1, (int*)matrix2) );
+
     return 0;
 }
 
@@ -49,17 +50,14 @@ void print_matrix(int* matrix)
     printf("\n");
 }
 
-int* multiplication_2_matrix(int* matrix1, int* matrix2)
+int* addition_2_matrix(int* matrix1, int* matrix2)
 {
     static int matrix_result[lines][columns] = {};
     for (int y = 0; y < lines; y++)
     {
         for (int x = 0; x < columns; x++)
         { 
-            for (int k = 0; k < columns; k++)
-            {
-                matrix_result[y][x] += (*(matrix1 + y * columns + k) * *(matrix2 + k * columns + x));
-            }
+            matrix_result[y][x] = *(matrix1 + y * columns + x) + *(matrix2 + y * columns + x);
         }
     }
     return (int*)matrix_result;
