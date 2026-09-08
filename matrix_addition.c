8249830be2a63c3ffd955ea500d1563f1e83e103
pso11 <sofiya.pshenichnaya.09@mail.ru>
2026-09-08 11:29:30 +0300

    +проверка на входные даные

--- matrix_addition.c
+++ matrix_addition.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void print_matrix(int* matrix);
+void print_matrix(int* matrix, int lines, int columns);
 int* addition_2_matrix(int* matrix1, int* matrix2);
 
-const int lines = 5, columns = 4;
+const int lines_1 = 5, columns_1 = 4, lines_2 = 5, columns_2 = 4, table_width = 5;
 
 int main(void)
 {
-    int matrix1[lines][columns] =
+    if (lines_1 != lines_2 && columns_1 != columns_2)
+    {
+        printf("You entered wrong data");
+        exit(EXIT_SUCCESS);
+    }
+
+    int matrix1[lines_1][columns_1] =
     {
         {0, 2, 3, 4},
         {11, 12, 13, 14}, 
@@ -16,7 +22,7 @@ int main(void)
         {31, 32, 33, 34},
         {41, 42, 43, 44}
     };
-    int matrix2[lines][columns] =
+    int matrix2[lines_2][columns_2] =
     {
         {2, 3, 4, 5},
         {12, 13, 14, 15}, 
@@ -26,24 +32,24 @@ int main(void)
     };
 
     printf("Matrix 1\n\n");
-    print_matrix((int*)matrix1);
+    print_matrix((int*)matrix1, lines_1, columns_1);
 
     printf("Matrix 2\n\n");
-    print_matrix((int*)matrix2);
+    print_matrix((int*)matrix2, lines_2, columns_2);
 
     printf("Result of folding to matrixes:\n\n");
-    print_matrix( addition_2_matrix((int*)matrix1, (int*)matrix2) );
+    print_matrix( addition_2_matrix((int*)matrix1, (int*)matrix2), lines_1, columns_1 );
 
     return 0;
 }
 
-void print_matrix(int* matrix)
+void print_matrix(int* matrix, int lines, int columns)
 {
     for (int y = 0; y < lines; y++)
     {
         for (int x = 0; x < columns; x++)
         {
-            printf("%5d ", *(matrix + y * columns + x));
+            printf("%*d ", table_width, *(matrix + y * columns + x));
         }
         printf("\n");
     }
@@ -52,12 +58,12 @@ void print_matrix(int* matrix)
 
 int* addition_2_matrix(int* matrix1, int* matrix2)
 {
-    static int matrix_result[lines][columns] = {};
-    for (int y = 0; y < lines; y++)
+    static int matrix_result[lines_1][columns_1] = {};
+    for (int y = 0; y < lines_1; y++)
     {
-        for (int x = 0; x < columns; x++)
+        for (int x = 0; x < columns_1; x++)
         { 
-            matrix_result[y][x] = *(matrix1 + y * columns + x) + *(matrix2 + y * columns + x);
+            matrix_result[y][x] = *(matrix1 + y * columns_1 + x) + *(matrix2 + y * columns_1 + x);
         }
     }
     return (int*)matrix_result;
